a95dea4d28c890140d2e81f98d0d47c5abf5f61e
Gabriel Figueira <gabriel.figueira@tecnico.ulisboa.pt>
2020-05-01 17:05:20 +0100

    Double and add algorithm

--- util.py
+++ util.py
@@ -44,6 +44,7 @@ def xgcd(a, b):
         a = -a
     while a != 0:
         q, a, b = complexDivision(b, a), modulo(b, a), a
+#        print(str(q) + " " + str(a) + " " + str(b))
         y0, y1 = y1, y0 - q * y1
         x0, x1 = x1, x0 - q * x1
     return b, x0, y0
@@ -51,7 +52,7 @@ def xgcd(a, b):
 def modularInverse(number, mod):
     g, x, _ = xgcd(number, mod)
     if g != 1:
-        raise ValueError(str(g) + "Isnt mod a prime?")
+        raise ValueError(str(x) + " " + str(g) + " Isnt mod a prime?")
     return modulo(x, mod)
 
 #Elliptic curve operations
@@ -69,7 +70,7 @@ def addPoint(p, q, curve, mod):
     elif modulo(p.x,mod) == modulo(q.x,mod) and modulo(q.y,mod) == modulo(-p.y,mod):
         return PointAtInfinity()
 
-    slope = modulo((p.y - q.y) * modularInverse(p.x - q.x, mod), mod)
+    slope = modulo((q.y - p.y) * modularInverse(q.x - p.x, mod), mod)
     x = modulo((slope ** 2 - p.x - q.x), mod)
     y = modulo((slope * (p.x - x) - p.y), mod)
     return Point(x,y)
@@ -85,10 +86,23 @@ def doublePoint(p, curve, mod):
 def negatePoint(p):
     return Point(p.x,-p.y)
 
-# Miller Algorithm
-
 def getBinary(integer):
     return [int(n) for n in bin(integer)[2:]]
+
+def doubleAndAdd(p, k, curve, mod):
+    binary = getBinary(k)
+    q = PointAtInfinity()
+    v = p
+    i = len(binary) - 1
+    while i >= 0:
+        if binary[i] == 1:
+            q = addPoint(q, v, curve, mod)
+        v = doublePoint(v, curve, mod)
+        i -= 1
+    return q
+
+
+# Miller Algorithm
 
 def computeFunction(p, q, value, curve, mod):
     if modulo(p.x,mod) == modulo(q.x,mod) and modulo(p.y,mod) == modulo(q.y,mod) and modulo(p.y,mod) != 0:
@@ -97,11 +111,11 @@ def computeFunction(p, q, value, curve, mod):
         return computeVertical(p, value, mod)
     else:
         slope = modulo((p.y - q.y) * modularInverse(p.x - q.x, mod), mod)
-        return modulo((value.y - p.y - slope * (value.x - p.x)) * modularInverse(value.x + p.x + q.x - slope **2, mod), mod)
+        return modulo(value.y - p.y - slope * (p.x - value.x) , mod)
 
 def computeTangent(p, value, curve, mod):
     slope = modulo(((3 * p.x ** 2 + curve.a) * modularInverse(2 * p.y,mod)), mod)
-    return modulo((-slope * value.x + value.y - p.y + slope * p.x) * modularInverse(value.x + 2 * p.x - slope**2, mod), mod)
+    return modulo(value.y - p.y + slope * (p.x - value.x), mod)
 
 def computeVertical(p, value, mod):
     return modulo((value.x - p.x), mod)
@@ -112,28 +126,24 @@ def Miller(p, order, value, curve, mod):
     binary = getBinary(order)
     i = len(binary) - 1
     while i >= 0:
+        dv = doublePoint(v, curve, mod)
         res = modulo(res ** 2 * computeFunction(v, v, value, curve, mod), mod)
-        v = doublePoint(p, curve, mod)
+        v = dv
         if binary[i] == 1:
+            vp = addPoint(v, p, curve, mod)
             res = modulo(res * computeFunction(v, p, value, curve, mod), mod)
-            v = addPoint(v, p, curve, mod)
+            v = vp
         i = i - 1
     return res
 
-p = Point(8, 703)
+p = Point(8,703)
 q = Point(49,20)
 s = Point(0,0)
 
-def WeilPairing(p, q, s, order, curve, mod):
-    a = Miller(p,7,addPoint(q,s,curve,mod),curve,mod)
-    b = Miller(p,7,s,curve,mod)
-    c = Miller(q,7,addPoint(p,negatePoint(s),curve,mod),curve,mod)
-    d = Miller(q,7,negatePoint(s),curve,mod)
-    
+def WeilPairing(p, q, order, curve, mod):
+    a = Miller(p,order,q,curve,mod)
+    b = Miller(q,order,p,curve,mod)
+    return modulo(a * modularInverse(b,mod),mod)
 
-    e = Miller(p,7,q,curve,mod)
-    f = Miller(q,7,p,curve,mod)
-    print(modulo(e * modularInverse(f,mod),mod))
-    return modulo(a * d * modularInverse(b * c, mod), mod)
-
-print(WeilPairing(p,q,s,7,EllipticCurve(37,0),1009)) 
+print(WeilPairing(p,q,7,EllipticCurve(37,0),1009)) 
+print(WeilPairing(q,p,7,EllipticCurve(37,0),1009)) 
