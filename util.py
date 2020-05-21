e3c89a6bb95f603ba0bcf8545089d9bd49aaa0d9
badjelix <miguel.ptfg@hotmail.com>
2020-05-21 19:42:03 +0100

    double and add seems fine

--- util.py
+++ util.py
@@ -4,23 +4,29 @@ import math
 from pynitefields import *
 
 class Point:
-
-    def __init__(self,x,y):
+    def __init__(self,x=0,y=0):
         self.x = x
         self.y = y
+
     def isInfinity(self):
         return False
 
-class PointAtInfinity(Point):
+    def printPoint(self):
+        if(self.isInfinity()):
+            print("Point at Infinity")
+        else:
+            print("X: " + convertListToString(self.x.exp_coefs) + "  Y: " + convertListToString(self.y.exp_coefs))
 
-    def __init__(self, prime):
-        gf = GaloisField(prime)
-        super().__init__(gf[0],gf[0])
+
+class PointAtInfinity(Point):
+    def __init__(self):
+        super().__init__()
+
     def isInfinity(self):
         return True
 
-class EllipticCurve:
 
+class EllipticCurve:
     def __init__(self,a,b):
         self.a = a
         self.b = b
@@ -37,7 +43,7 @@ def addPoint(p, q, curve):
     elif q.isInfinity():
         return p
     elif p.x == q.x and p.y == q.y - 2 * q.y:
-        return PointAtInfinity(p.x.p)
+        return PointAtInfinity()
     elif p.x == q.x and q.y == p.y:
         slope = (3 * pow(p.x, 2) + curve.a) / (2 * p.y)
     else:
@@ -46,23 +52,20 @@ def addPoint(p, q, curve):
     y = slope * (p.x - x) - p.y
     return Point(x,y)
 
-def negatePoint(p):
-    return Point(p.x,p.y - 2 * p.y)
-
 def getBinary(integer):
     return [int(n) for n in bin(integer)[2:]]
 
-def doubleAndAdd(p, k, curve):
-    binary = getBinary(k)
-    q = PointAtInfinity()
-    v = p
+def doubleAndAdd(p, n, curve):
+    binary = getBinary(n)
+    r = PointAtInfinity()
+    q = p
     i = len(binary) - 1
     while i >= 0:
         if binary[i] == 1:
-            q = addPoint(q, v, curve)
-        v = addPoint(v, v, curve)
+            r = addPoint(r, q, curve)
+        q = addPoint(q, q, curve)
         i -= 1
-    return q
+    return r
 
 
 # Miller Algorithm
@@ -101,8 +104,8 @@ def WeilPairing(p, q, s, order, curve):
     b = Miller(p,order,s,curve)
     c = Miller(q,order,addPoint(p, negatePoint(s), curve),curve)
     d = Miller(q,order,negatePoint(s),curve)
-   
-    
+
+
     #e = Miller(p,order,q,curve)
     #f = Miller(q,order,p,curve)
     #print(e)
@@ -112,16 +115,28 @@ def WeilPairing(p, q, s, order, curve):
     print(b)
     print(c)
     print(d)
-    return a * d / (b * c) 
+    return a * d / (b * c)
+
+
+## Main and other functions
+
+def convertListToString(input_seq):
+    i = 0
+    final_str = "["
+    while(True):
+        final_str += " " + str(input_seq[i]) + " "
+        i += 1
+        if(i == len(input_seq)):
+            final_str += "]"
+            break
+        final_str += ";"
+    return final_str
 
 if __name__ == "__main__":
-    gf = GaloisField(1009)
-    p = Point(gf[8],gf[703])
-    q = Point(gf[49],gf[20])
-    s = Point(gf[0],gf[0])
-    ec = EllipticCurve(gf[37],gf[0])
+    gf = GaloisField(17)
+    p = Point(gf[9],gf[15])
+    #q = Point(gf[49],gf[20])
+    #s = Point(gf[0],gf[0])
+    ec = EllipticCurve(gf[0],gf[7])
 
-
-    print(WeilPairing(p,q,s,7,ec)) 
-    print(WeilPairing(p,p,s,7,ec))
-
+    doubleAndAdd(p,15,ec).printPoint()
